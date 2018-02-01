a841ad8f5974cc38c5619132fec7b49e910f6c1d
Paul Cuichan <sistemas@linkear.net>
2018-02-01 15:31:16 -0500

    Home construction

--- src/app/public/app-routing.module.ts
+++ src/app/public/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PublicComponent } from './public.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: PublicComponent },
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
  
 ];
